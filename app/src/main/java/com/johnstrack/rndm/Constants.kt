a9c3a1814313868713cb84b6e569d24a85403ee9
John Strack <Braveheart22@GMail.com>
2018-04-30 20:36:15 -0400

    Create Constants.kt

---
+++ app/src/main/java/com/johnstrack/rndm/Constants.kt
@@ -0,0 +1,11 @@
+package com.johnstrack.rndm
+
+// Category constants
+
+const val FUNNY = "funny"
+const val SERIOUS = "serious"
+const val CRAZY = "crazy"
+
+// Collection reference
+
+const val   THOUGHTS_REF = "thoughts"
