4a71832ccb16974e50c98eb886fa23e8c7da8f66
John Strack <Braveheart22@GMail.com>
2018-05-02 16:11:28 -0400

    Implement filter buttons on main activity

--- app/src/main/java/com/johnstrack/rndm/MainActivity.kt
+++ app/src/main/java/com/johnstrack/rndm/MainActivity.kt
@@ -7,9 +7,10 @@ import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.View
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import java.sql.Timestamp
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var thoughtsAdapter: ThoughtsAdapter
     private val thoughts = arrayListOf<Thought>()
     private val thoughtsCollectRef = FirebaseFirestore.getInstance().collection(THOUGHTS_REF)
+    private lateinit var thoughtsListener: ListenerRegistration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,28 +33,75 @@ class MainActivity : AppCompatActivity() {
 
         thoughtsAdapter = ThoughtsAdapter(thoughts)
         thoughtListView.adapter = thoughtsAdapter
-        val layoutManager = LinearLayoutManager (this)
+        val layoutManager = LinearLayoutManager(this)
         thoughtListView.layoutManager = layoutManager
+    }
 
-        thoughtsCollectRef.get()
-                .addOnSuccessListener {snapshot ->
-                    for (document in snapshot.documents) {
-                        val data = document.data
-                        println(data.toString())
-                        val name = data!![USERNAME] as String
-                        val timestamp = data[TIMESTAMP] as Date
-                        val thoughtTxt = data[THOUGHT_TXT] as String
-                        val numLikes = data[NUM_LIKES] as Long
-                        val numComments = data[NUM_COMMENTS] as Long
-                        val documentId = document.id
+    override fun onResume() {
+        super.onResume()
+        setListener()
+    }
 
-                        val newThought = Thought(name, timestamp, thoughtTxt, numLikes.toInt(), numComments.toInt(), documentId)
-                        thoughts.add(newThought)
+    private fun setListener() {
+
+        if (selectedCategory == POPULAR) {
+            thoughtsListener = thoughtsCollectRef
+                    .orderBy(NUM_LIKES, Query.Direction.DESCENDING)
+                    .addSnapshotListener(this) { snapshot, exception ->
+
+                        if (exception != null) {
+                            Log.e("Exception", "Could not retrieve documetns: ${exception.localizedMessage}")
+                        }
+
+                        if (snapshot != null) {
+
+                            thoughts.clear()
+
+                            for (document in snapshot?.documents!!) {
+                                val data = document.data
+                                val name = data!![USERNAME] as String
+                                val timestamp = data[TIMESTAMP] as Date
+                                val thoughtTxt = data[THOUGHT_TXT] as String
+                                val numLikes = data[NUM_LIKES] as Long
+                                val numComments = data[NUM_COMMENTS] as Long
+                                val documentId = document.id
+
+                                val newThought = Thought(name, timestamp, thoughtTxt, numLikes.toInt(), numComments.toInt(), documentId)
+                                thoughts.add(newThought)
+                            }
+                            thoughtsAdapter.notifyDataSetChanged()
+                        }
                     }
-                    thoughtsAdapter.notifyDataSetChanged()
-                }.addOnFailureListener { exception ->
-                    Log.e("Exception", "Could not get thoughts ${exception.localizedMessage}")
-                }
+        } else {
+            thoughtsListener = thoughtsCollectRef
+                    .orderBy(TIMESTAMP, Query.Direction.DESCENDING)
+                    .whereEqualTo(CATEGORY, selectedCategory)
+                    .addSnapshotListener(this) { snapshot, exception ->
+
+                        if (exception != null) {
+                            Log.e("Exception", "Could not retrieve documetns: ${exception.localizedMessage}")
+                        }
+
+                        if (snapshot != null) {
+
+                            thoughts.clear()
+
+                            for (document in snapshot?.documents!!) {
+                                val data = document.data
+                                val name = data!![USERNAME] as String
+                                val timestamp = data[TIMESTAMP] as Date
+                                val thoughtTxt = data[THOUGHT_TXT] as String
+                                val numLikes = data[NUM_LIKES] as Long
+                                val numComments = data[NUM_COMMENTS] as Long
+                                val documentId = document.id
+
+                                val newThought = Thought(name, timestamp, thoughtTxt, numLikes.toInt(), numComments.toInt(), documentId)
+                                thoughts.add(newThought)
+                            }
+                            thoughtsAdapter.notifyDataSetChanged()
+                        }
+                    }
+        }
     }
 
     fun mainFunnyClicked(view: View) {
@@ -61,6 +110,9 @@ class MainActivity : AppCompatActivity() {
         mainCrazyBtn.isChecked = false
         mainPopularBtn.isChecked = false
         selectedCategory = FUNNY
+
+        thoughtsListener.remove()
+        setListener()
     }
 
     fun mainSeriousClicked(view: View) {
@@ -69,6 +121,9 @@ class MainActivity : AppCompatActivity() {
         mainCrazyBtn.isChecked = false
         mainPopularBtn.isChecked = false
         selectedCategory = SERIOUS
+
+        thoughtsListener.remove()
+        setListener()
     }
 
     fun mainCrazyClicked(view: View) {
@@ -77,6 +132,9 @@ class MainActivity : AppCompatActivity() {
         mainCrazyBtn.isChecked = true
         mainPopularBtn.isChecked = false
         selectedCategory = CRAZY
+
+        thoughtsListener.remove()
+        setListener()
     }
 
     fun mainPopularClicked(view: View) {
@@ -85,5 +143,8 @@ class MainActivity : AppCompatActivity() {
         mainCrazyBtn.isChecked = false
         mainPopularBtn.isChecked = true
         selectedCategory = POPULAR
+
+        thoughtsListener.remove()
+        setListener()
     }
 }
