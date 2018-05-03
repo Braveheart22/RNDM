109063f6b6922c8e104db347b2045a2365d94f66
John <Braveheart22@GMail.com>
2018-05-02 21:29:32 -0400

    Finish login and create user layouts

--- app/src/main/java/com/johnstrack/rndm/CreateUserActivity.kt
+++ app/src/main/java/com/johnstrack/rndm/CreateUserActivity.kt
@@ -2,11 +2,20 @@ package com.johnstrack.rndm
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CreateUserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+    }
+
+    fun createCreatedClicked (view: View) {
+
+    }
+
+    fun createCancelClicked (view: View) {
+
     }
 }
--- app/src/main/java/com/johnstrack/rndm/LoginActivity.kt
+++ app/src/main/java/com/johnstrack/rndm/LoginActivity.kt
@@ -2,11 +2,20 @@ package com.johnstrack.rndm
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginClicked (view: View) {
+
+    }
+
+    fun loginCreateClicked (view: View) {
+
     }
 }
